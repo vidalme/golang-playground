65bc507fd951aacadfb200ce5634c6d64466bb7d
andre <andrinho@gmail.com>
2024-08-27 08:59:38 -0300

    progresso curso 03 alura

--- cursos/alura/03/main.go
+++ cursos/alura/03/main.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -13,5 +20,12 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
+		{"Tenis", "Confortavel", 89, 3},
+		{"Fone", "Muito bom", 59, 2},
+		{"Prancha de surf", "radical", 899, 1},
+	}
+	temp.ExecuteTemplate(w, "Index", produtos)
 }

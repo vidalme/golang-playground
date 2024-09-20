a1774a558e9fa3b8f092e776867918d7ab737155
andre <andrinho@gmail.com>
2024-09-20 03:57:25 -0300

    just updating go studying progress

---
+++ cursos/testing-TrevorSawler/webapp/cmd/web/routes.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// register middleware
+	mux.Use(middleware.Recoverer)
+
+	// register routes
+	mux.Get("/", app.Home)
+
+	// static assets
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
+	return mux
+}

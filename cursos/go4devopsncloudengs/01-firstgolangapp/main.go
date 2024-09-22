c387fc30e8a2edda0731e776e28be3be099719a9
andre <andrinho@gmail.com>
2024-09-21 23:04:18 -0300

    go4devops udemy course, beginning

---
+++ cursos/go4devopsncloudengs/01-firstgolangapp/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+func main() {
+	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Printf("Usage ./http-get <url>\n")
+		os.Exit(1)
+	}
+
+	if _, err := url.ParseRequestURI(args[1]); err != nil {
+		fmt.Printf("URL is a invalid format: %s>\n", err)
+		os.Exit(1)
+	}
+
+	response, err := http.Get(args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("HTTP Status Code: %d\n Body:%s \n", response.StatusCode, body)
+}

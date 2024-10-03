a45df2cd444a5c2b94e71bd9fa7902e8b4197847
andre <andrinho@gmail.com>
2024-10-03 08:03:16 -0300

    ascan GO module progress

---
+++ cursos/ascan/02_composition_interfaces_main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type reader interface {
+	read() (int, error)
+}
+
+type writer interface {
+	write() (int, error)
+}
+
+type readWriter interface {
+	reader
+	writer
+}
+
+type printer struct {
+	name string
+}
+
+func (p printer) read() (int, error) {
+	fmt.Println("------------")
+	fmt.Println("metodo read")
+	fmt.Println("------------")
+	return 1, nil
+}
+
+func (p printer) write() (int, error) {
+	fmt.Println("------------")
+	fmt.Println("metodo write")
+	fmt.Println("------------")
+	return 1, nil
+}
+
+func main() {
+	p := printer{"hp"}
+	var rw readWriter = p
+	rw.read()
+	rw.write()
+}
